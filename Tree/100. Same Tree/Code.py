3dd5790f8368917c10c32dfc8447cdbf10c90773
Shivam Vats <123102767+shivam37483@users.noreply.github.com>
2025-04-13 15:51:00 +0530

    Update and rename Code to Code.py

--- Tree/100. Same Tree/Code.py
+++ Tree/100. Same Tree/Code.py
@@ -6,10 +6,16 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if not p and not q:
-            return True
-        
+        # Intuition: filter out all the cases where we dont have both the nodes with same val.
+        if (not p and not q): return True
+
+        # Only continue the iteration when we have both non-null nodes with same values
         if p and q and p.val == q.val:
             return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
-        else:
-            return False
+
+        # Return False for anything else
+        return False
+    
+
+# O(n)
+# O(n)      | The total space used is the number of stack frames times the space per frame -> O(1) * O(n)
